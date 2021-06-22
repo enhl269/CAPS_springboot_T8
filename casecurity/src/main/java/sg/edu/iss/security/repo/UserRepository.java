56e8b1e3d44a18f850c17e5aebc202ff11b7ff83
tansizheng89 <e0696890@u.nus.edu>
2021-06-22 22:27:39 +0800

    simple test cases for users

--- casecurity/src/main/java/sg/edu/iss/security/repo/UserRepository.java
+++ casecurity/src/main/java/sg/edu/iss/security/repo/UserRepository.java
@@ -1,9 +1,10 @@
 package sg.edu.iss.security.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import sg.edu.iss.security.domain.Admin;
 import sg.edu.iss.security.domain.User;
 
 //to change to User class to test it out whether it still persist to the database or not
@@ -11,5 +12,8 @@ import sg.edu.iss.security.domain.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("SELECT u FROM User u WHERE u.email = ?1")
 	public User findByEmail(String email);
+	
+	@Query("select u from User u where u.roles = ?1")
+	public List<User> findByRoleType(String role);
 	
 }
